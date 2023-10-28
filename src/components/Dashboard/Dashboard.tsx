03d18bcccafdc5afda05292b591d3269e332768b
Ander <20172991@aloe.ulima.edu.pe>
2023-10-27 22:39:58 -0500

    dashboard layout

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 import styles from './Dashboard.module.scss'
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className={styles.dashboard}>
+      <div className={styles.dashboard_top}>
+        <p>ingresos y gastos</p>
+        <p>dashboard grafico</p>
+      </div>
+      <div className={styles.dashboard_bot}>
+        <p>cuy</p>
+        <p>otro coso</p>
+      </div>
+    </div>
   )
 }
 
