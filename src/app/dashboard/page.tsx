40eb245739e3371825c7cf63e47017fe2d8d7e48
erojas <erojas@ghx.com>
2023-10-28 05:53:06 +0200

    feat:add card curso

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,14 @@
 import Sidebar from '@/components/Sidebar/Sidebar'
 import React from 'react'
 import styles from './dashboard.module.scss'
+import CardCurso from '@/components/card-curso/card-curso'
 const Dashboard = () => {
   return (
     <div className={styles.dashboard}>
         <Sidebar/>
         <div>Derecha</div>
+        <CardCurso/>
+
     </div>
   )
 }
