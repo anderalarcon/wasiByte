19071fff3e3fd04dbd54eae01a7c7d5e4ede53c8
unknown <vgabrielcv@gmail.com>
2023-10-28 00:34:01 -0500

    Added topbar 2

---
+++ src/components/TopBar/TopBar.tsx
@@ -0,0 +1,26 @@
+import { Bell, ChevronDown } from 'lucide-react'
+import React from 'react'
+
+const TopBar = () => {
+    return (
+        <nav className='py-5 px-10 border-b-2 border-gray-200'>
+            <div className='flex justify-between items-center'>
+                <div className="text-lg font-semibold">
+                    <h1>Bienvenido! ☀️</h1>
+                </div>
+
+                <div className="flex items-center space-x-5">
+                    <div className='hover:bg-gray-100 p-2 rounded-full cursor-pointer'>
+                        <Bell />
+                    </div>
+                    <div className="flex items-center space-x-2 p-2 rounded-lg">
+                        <div className="h-8 w-8 rounded-full bg-slate-200" />
+                        <span className="text-sm font-medium">Roger Vega</span>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default TopBar
